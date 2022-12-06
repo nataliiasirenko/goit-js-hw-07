84c89af0ff877117dab75363453edb1ce0c12fcd
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-12-06 13:37:16 +0000

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,73 +1,42 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const gallery = document.querySelector(`.gallery`);
-const items = galleryItems
-  .map(({ preview, original, description }) => {
-    return `
-      <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </div>`;
-  })
-  .join('');
 
-gallery.insertAdjacentHTML(`afterbegin`, items);
+const makeGalerryItemsMarkup = ({ preview, original, description }) => {
+  return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
 
-gallery.addEventListener(`click`, onGalleryItemClick);
+    />
+  </a>
+</div>`;
+};
 
-function onGalleryItemClick(event) {
-  event.preventDefault();
-  if (!event.target.classList.includes('gallery__item')) {
+const makeGalerryItems = galleryItems.map(makeGalerryItemsMarkup).join('');
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', makeGalerryItems);
+galleryEl.addEventListener('click', handleImageClick);
+
+function handleImageClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
     return;
   }
-  const instance = basicLightbox.create(`
-    <img src="${e.target.dataset.src}" width="800" height="600">
-`);
-  instance.show();
-  galerry.addEventListener(`keydown`, e => {
-    if (e.key === `Escape`) {
+  //   const instance = basicLightbox.create(`
+  //   <img src="${e.target.dataset.source}" width -"800" height-"600">
+  // `);
+  //   instance.show();
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('show.simplelightbox', function () {
+    // do something…
+  });
+
+  galleryEl.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
       instance.close();
     }
   });
 }
-
-// function onModalEscapeClick(evt) {
-//   if (evt.key === `Escape`) {
-//     instance.close();
-//   }
-// }
-
-// const basicLightBox = document.querySelector(`.gallery`)
-// gallery.addEventListener(`click`, e => {
-//   instance = basicLightBox.create(
-//     `<img src ="${e.target.src}` >
-//       {
-//         onShow: () => document.addEventListener('keydown', onModalEscapeClick),
-//         onClose: () => document.removeEventListener('keydown', onModalEscapeClick),
-//       }
-//   );
-//   instance.show();
-// });
-
-// const instance = basicLightbox.create(
-//   `
-//     <div class="modal">
-//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
-//         <input placeholder="Type something">
-//         <a>Close</a>
-//     </div>
-// `,
-//   {
-//     onShow: instance => {
-//       instance.element().querySelector('a').onclick = instance.close;
-//     },
-//   }
-// );
-
-// instance.show();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
 
-const lightbox = new SimpleLightbox('.galleryItems a', {
-  /* options */
+const galleryMarkup = createGalleyMarkup(galleryItems);
+gallery.insertAdjacentHTML(`beforeend`, galleryMarkup);
+
+function createGalleyMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__item"
+href="${original}">
+  <img class="gallery__image"
+  src="${preview}" 
+  alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionData: `alt`,
+  captionDelay: 250,
 });
